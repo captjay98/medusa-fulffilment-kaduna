61962d8ceb43732c8a38d88374d99b6a6c4ca093
captjay98 <captjay98@gmail.com>
2024-02-21 15:42:40 +0100

    pricing fix

--- src/services/kaduna-fulfillment.ts
+++ src/services/kaduna-fulfillment.ts
@@ -19,163 +19,179 @@ class KadunaFulfillmentService extends AbstractFulfillmentService {
 
   deliveryPrices: DeliveryPrices = {
     Abia: {
-      "kaduna-fulfillment-public": 1200,
-      "kaduna-fulfillment-gig": 1800,
+      "kaduna-fulfillment-public": 120000,
+      "kaduna-fulfillment-gig": 180000,
     },
 
-    Adamawa: {
-      "kaduna-fulfillment-public": 1300,
-      "kaduna-fulfillment-gig": 1900,
+    adamawa: {
+      "kaduna-fulfillment-public": 130000,
+      "kaduna-fulfillment-gig": 190000,
     },
 
-    "Akwa Ibom": {
-      "kaduna-fulfillment-public": 1400,
-      "kaduna-fulfillment-gig": 2000,
+    "akwa ibom": {
+      "kaduna-fulfillment-public": 140000,
+      "kaduna-fulfillment-gig": 200000,
     },
 
-    Anambra: {
-      "kaduna-fulfillment-public": 1500,
-      "kaduna-fulfillment-gig": 2100,
+    anambra: {
+      "kaduna-fulfillment-public": 150000,
+      "kaduna-fulfillment-gig": 210000,
     },
 
-    Bauchi: {
-      "kaduna-fulfillment-public": 1600,
-      "kaduna-fulfillment-gig": 2200,
+    bauchi: {
+      "kaduna-fulfillment-public": 160000,
+      "kaduna-fulfillment-gig": 220000,
     },
 
-    Bayelsa: {
-      "kaduna-fulfillment-public": 1700,
-      "kaduna-fulfillment-gig": 2300,
+    bayelsa: {
+      "kaduna-fulfillment-public": 170000,
+      "kaduna-fulfillment-gig": 230000,
     },
-    Benue: {
-      "kaduna-fulfillment-public": 1800,
-      "kaduna-fulfillment-gig": 2400,
+    benue: {
+      "kaduna-fulfillment-public": 180000,
+      "kaduna-fulfillment-gig": 240000,
     },
 
-    Borno: {
-      "kaduna-fulfillment-public": 1900,
-      "kaduna-fulfillment-gig": 2500,
+    borno: {
+      "kaduna-fulfillment-public": 190000,
+      "kaduna-fulfillment-gig": 250000,
     },
     "Cross River": {
-      "kaduna-fulfillment-public": 2000,
-      "kaduna-fulfillment-gig": 2600,
+      "kaduna-fulfillment-public": 200000,
+      "kaduna-fulfillment-gig": 260000,
     },
-    Delta: {
-      "kaduna-fulfillment-public": 2100,
-      "kaduna-fulfillment-gig": 2700,
+    delta: {
+      "kaduna-fulfillment-public": 210000,
+      "kaduna-fulfillment-gig": 270000,
     },
-    Ebonyi: {
-      "kaduna-fulfillment-public": 2200,
-      "kaduna-fulfillment-gig": 2800,
+    ebonyi: {
+      "kaduna-fulfillment-public": 220000,
+      "kaduna-fulfillment-gig": 280000,
     },
-    Edo: {
-      "kaduna-fulfillment-public": 2300,
-      "kaduna-fulfillment-gig": 2900,
+    edo: {
+      "kaduna-fulfillment-public": 230000,
+      "kaduna-fulfillment-gig": 290000,
     },
     Ekiti: {
-      "kaduna-fulfillment-public": 2400,
-      "kaduna-fulfillment-gig": 3000,
+      "kaduna-fulfillment-public": 240000,
+      "kaduna-fulfillment-gig": 300000,
     },
 
-    Enugu: {
-      "kaduna-fulfillment-public": 2500,
-      "kaduna-fulfillment-gig": 3100,
+    enugu: {
+      "kaduna-fulfillment-public": 250000,
+      "kaduna-fulfillment-gig": 310000,
     },
 
-    Gombe: {
-      "kaduna-fulfillment-public": 2600,
-      "kaduna-fulfillment-gig": 3200,
+    gombe: {
+      "kaduna-fulfillment-public": 260000,
+      "kaduna-fulfillment-gig": 320000,
     },
-    Imo: { "kaduna-fulfillment-public": 2700, "kaduna-fulfillment-gig": 3300 },
-
-    Jigawa: {
-      "kaduna-fulfillment-public": 2800,
-      "kaduna-fulfillment-gig": 3400,
-    },
-    Kaduna: {
-      "kaduna-fulfillment-public": 2000,
-      "kaduna-fulfillment-gig": 4000,
+    imo: {
+      "kaduna-fulfillment-public": 270000,
+      "kaduna-fulfillment-gig": 330000,
     },
 
-    Kano: {
-      "kaduna-fulfillment-public": 2100,
-      "kaduna-fulfillment-gig": 4100,
+    jigawa: {
+      "kaduna-fulfillment-public": 280000,
+      "kaduna-fulfillment-gig": 340000,
     },
-    Katsina: {
-      "kaduna-fulfillment-public": 2200,
-      "kaduna-fulfillment-gig": 4200,
+    kaduna: {
+      "kaduna-fulfillment-public": 200000,
+      "kaduna-fulfillment-gig": 400000,
     },
-    Kebbi: {
-      "kaduna-fulfillment-public": 2300,
-      "kaduna-fulfillment-gig": 4300,
+
+    kano: {
+      "kaduna-fulfillment-public": 210000,
+      "kaduna-fulfillment-gig": 410000,
+    },
+
+    katsina: {
+      "kaduna-fulfillment-public": 220000,
+      "kaduna-fulfillment-gig": 420000,
+    },
+
+    kebbi: {
+      "kaduna-fulfillment-public": 230000,
+      "kaduna-fulfillment-gig": 430000,
     },
     Kogi: {
-      "kaduna-fulfillment-public": 2400,
-      "kaduna-fulfillment-gig": 4400,
-    },
-    Kwara: {
-      "kaduna-fulfillment-public": 2500,
-      "kaduna-fulfillment-gig": 4500,
-    },
-    Lagos: {
-      "kaduna-fulfillment-public": 800,
-      "kaduna-fulfillment-gig": 1500,
-    },
-    Nasarawa: {
-      "kaduna-fulfillment-public": 2600,
-      "kaduna-fulfillment-gig": 4600,
-    },
-    Niger: {
-      "kaduna-fulfillment-public": 2700,
-      "kaduna-fulfillment-gig": 4700,
-    },
-    Ogun: {
-      "kaduna-fulfillment-public": 2800,
-      "kaduna-fulfillment-gig": 4800,
-    },
-    Ondo: {
-      "kaduna-fulfillment-public": 2900,
-      "kaduna-fulfillment-gig": 4900,
-    },
-    Osun: {
-      "kaduna-fulfillment-public": 3000,
-      "kaduna-fulfillment-gig": 5000,
-    },
-    Oyo: {
-      "kaduna-fulfillment-public": 3100,
-      "kaduna-fulfillment-gig": 5100,
-    },
-    Plateau: {
-      "kaduna-fulfillment-public": 3200,
-      "kaduna-fulfillment-gig": 5200,
-    },
-    Rivers: {
-      "kaduna-fulfillment-public": 3300,
-      "kaduna-fulfillment-gig": 5300,
-    },
-    Sokoto: {
-      "kaduna-fulfillment-public": 3400,
-      "kaduna-fulfillment-gig": 5400,
+      "kaduna-fulfillment-public": 240000,
+      "kaduna-fulfillment-gig": 440000,
     },
 
-    Taraba: {
-      "kaduna-fulfillment-public": 3500,
-      "kaduna-fulfillment-gig": 5500,
+    kwara: {
+      "kaduna-fulfillment-public": 250000,
+      "kaduna-fulfillment-gig": 450000,
     },
 
-    Yobe: {
-      "kaduna-fulfillment-public": 3600,
-      "kaduna-fulfillment-gig": 5600,
+    lagos: {
+      "kaduna-fulfillment-public": 800000,
+      "kaduna-fulfillment-gig": 150000,
     },
 
-    Zamfara: {
-      "kaduna-fulfillment-public": 3700,
-      "kaduna-fulfillment-gig": 5700,
+    nasarawa: {
+      "kaduna-fulfillment-public": 260000,
+      "kaduna-fulfillment-gig": 460000,
     },
 
-    "Federal Capital Territory (Abuja)": {
-      "kaduna-fulfillment-public": 3800,
-      "kaduna-fulfillment-gig": 5800,
+    niger: {
+      "kaduna-fulfillment-public": 270000,
+      "kaduna-fulfillment-gig": 470000,
+    },
+
+    ogun: {
+      "kaduna-fulfillment-public": 280000,
+      "kaduna-fulfillment-gig": 480000,
+    },
+
+    ondo: {
+      "kaduna-fulfillment-public": 290000,
+      "kaduna-fulfillment-gig": 490000,
+    },
+
+    osun: {
+      "kaduna-fulfillment-public": 300000,
+      "kaduna-fulfillment-gig": 500000,
+    },
+
+    oyo: {
+      "kaduna-fulfillment-public": 310000,
+      "kaduna-fulfillment-gig": 510000,
+    },
+
+    plateau: {
+      "kaduna-fulfillment-public": 320000,
+      "kaduna-fulfillment-gig": 520000,
+    },
+
+    rivers: {
+      "kaduna-fulfillment-public": 330000,
+      "kaduna-fulfillment-gig": 530000,
+    },
+
+    sokoto: {
+      "kaduna-fulfillment-public": 340000,
+      "kaduna-fulfillment-gig": 540000,
+    },
+
+    taraba: {
+      "kaduna-fulfillment-public": 350000,
+      "kaduna-fulfillment-gig": 550000,
+    },
+
+    yobe: {
+      "kaduna-fulfillment-public": 360000,
+      "kaduna-fulfillment-gig": 560000,
+    },
+
+    zamfara: {
+      "kaduna-fulfillment-public": 370000,
+      "kaduna-fulfillment-gig": 570000,
+    },
+
+    abuja: {
+      "kaduna-fulfillment-public": 380000,
+      "kaduna-fulfillment-gig": 580000,
     },
   };
 
@@ -199,10 +215,9 @@ class KadunaFulfillmentService extends AbstractFulfillmentService {
     data: Record<string, unknown>,
     cart: Cart,
   ): Promise<Record<string, unknown>> {
-    console.log("DATA!!", data);
     if (
-      data.id !== "kaduna-fulfillment-public" &&
-      data.id !== "kaduna-fulfillment-gig"
+      optionData.id !== "kaduna-fulfillment-public" &&
+      optionData.id !== "kaduna-fulfillment-gig"
     ) {
       throw new Error("Invalid fulfillment option ID");
     }
@@ -244,8 +259,9 @@ class KadunaFulfillmentService extends AbstractFulfillmentService {
       throw new Error("Delivery address does not have a city property");
     }
 
-    const state = address.city as string;
-    const selectedOption = data.id as string;
+    const state = address.city.toLowerCase() as string;
+    console.log(state);
+    const selectedOption = optionData.id as string;
 
     const statePrices = this.deliveryPrices[state];
     if (!statePrices) {
